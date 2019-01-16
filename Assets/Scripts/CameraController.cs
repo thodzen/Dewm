789092e9f9e108887288a77d0ad5861586dfab28
thodzen <thodzen@gmail.com>
2019-01-15 21:10:50 -0500

    Uploading what I've started
    Added movement, arm rotation, weapon firing

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public PlayerController player;
+
+    public bool isFollowing;
+
+    public Vector3 offset;
+
+    public float smoothing;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+
+        isFollowing = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector3 newPosition = Vector3.Lerp(transform.position, player.transform.position + offset, smoothing);
+        transform.position = newPosition;
+    }
+}
+
