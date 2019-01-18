6d4a1c8d2cc28851f43ad3102f3a64344cb08716
thodzen <38892841+thodzen@users.noreply.github.com>
2019-01-17 23:14:41 -0500

    Fixed Enemy Health
    Enemy can now take damage.

---
+++ Assets/Scripts/HurtEnemyOnContact.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtEnemyOnContact : MonoBehaviour {
+
+    public int damageToGive;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            other.GetComponent<EnemyHealthManager>().giveDamage(damageToGive);
+            Debug.Log("ouch");
+        }
+    }
+
+}
