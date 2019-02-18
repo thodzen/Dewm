d6f3f4251d20468521fccfbb9b0cced0856e7f19
Davidah121 <42045018+Davidah121@users.noreply.github.com>
2019-02-18 13:52:14 -0500

    Added Basic Doors
    The doors are tied to keycards by a number. There is a small basic animation for the door opening, but it plays as soon as you get the key and not when you are next to the door.

---
+++ Assets/KeyCardScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCardScript : MonoBehaviour
+{
+    public int keyCardID = 0;
+    public PlayerController p;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if(this.GetComponent<BoxCollider2D>().IsTouching( p.GetComponent<BoxCollider2D>()))
+        {
+            LevelManager.lm.setKeyOwned(keyCardID);
+            Destroy(gameObject);
+        }
+    }
+}
