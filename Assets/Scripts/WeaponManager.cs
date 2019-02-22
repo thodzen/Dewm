c853593b96bebdf36b12f955cdf123e1a86b1174
hadley31 <nick.hadley31@gmail.com>
2019-02-22 11:16:35 -0500

    updated scripting version; started weapon manager

---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    [SerializeField]
+    private Weapon weapon;
+
+
+    public Weapon Weapon
+    {
+        get { return this.weapon; }
+        set { Give(value); }
+    }
+
+
+    public void Give(Weapon weapon)
+    {
+        this.weapon?.gameObject?.SetActive(false);
+
+        this.weapon = weapon;
+    }
+}
