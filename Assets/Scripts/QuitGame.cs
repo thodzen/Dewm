3d1ca2ccfbf11046fe0891c60781ac09ab6ad75a
thodzen <thodzen@gmail.com>
2019-03-03 21:30:18 -0500

    Completed Tutorial Prototype

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+        Debug.Log("Application has ended.");
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+            Debug.Log("Application has ended.");
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public GameObject theManager;
+    public void LoadScene(string name)
+
+    {
+        SceneManager.LoadScene(name, LoadSceneMode.Single);
+        StartCoroutine(LoadCurrentScene());
+    }
+
+    IEnumerator LoadCurrentScene()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
