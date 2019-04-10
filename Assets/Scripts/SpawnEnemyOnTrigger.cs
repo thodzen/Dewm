1771ed912b24809e7319a59d267344fd22b54edc
thodzen <thodzen@gmail.com>
2019-04-10 13:53:21 -0400

    Added an Enemy Spawner and Platform Lights

---
+++ Assets/Scripts/SpawnEnemyOnTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemyOnTrigger : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform enemyPos;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Debug.Log(i + " enemies spawned");
+                Instantiate(enemyPrefab, enemyPos.position, enemyPos.rotation);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
