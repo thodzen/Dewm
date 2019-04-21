e6f148e1c15c339f61e50d4fb206afe3feddb8ac
thodzen <thodzen@gmail.com>
2019-04-21 16:41:50 -0400

    Lighting Fixed
    ty Alan

---
+++ Assets/LightScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+    private PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Light thisLight = GetComponent<Light>();
+        float dis = (thisLight.transform.position - player.transform.position).magnitude;
+        if (dis > thisLight.range)
+        {
+            thisLight.enabled = false;
+        }
+        else
+        {
+            thisLight.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/LoadSceneOnTouch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnTouch : MonoBehaviour
+{
+    public float delay = 440;
+
+    public string levelToLoad;
+
+    public string sceneName;
+
+    public AudioSource soundClip;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            soundClip.Play();
+            Invoke("DelayedAction", delay);
+        }
+    }
+
+    void DelayedAction()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
